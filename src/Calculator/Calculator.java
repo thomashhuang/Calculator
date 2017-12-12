1d320266d404601c7a9281563d3e1580e451b55e
Thomas Huang <thomas.huang2017@gmail.com>
2017-12-12 12:40:30 -0600

    Initial commit

---
+++ src/Calculator/Calculator.java
@@ -0,0 +1,32 @@
+package Calculator;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Calculator implements ActionListener {
+    
+    private JFrame calculatorWindow;
+    private JTextField text;
+    
+    private JButton one = new JButton("1");
+    private JButton two = new JButton("1");
+    private JButton three = new JButton("1");
+    private JButton four = new JButton("1");
+    private JButton five = new JButton("1");
+    private JButton six = new JButton("1");
+    private JButton seven = new JButton("1");
+    private JButton eight = new JButton("1");
+    private JButton nine = new JButton("1");
+    private JButton zero = new JButton("1");
+    private JButton decimal = new JButton("1");
+    private JButton plus = new JButton("1");
+    private JButton minus = new JButton("1");
+    private JButton times = new JButton("1");
+    private JButton divide = new JButton("1");
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
