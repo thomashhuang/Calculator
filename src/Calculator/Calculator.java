f5f7765fd444b6740c845950aaa99b6ff21cff4d
Thomas Huang <thomas.huang2017@gmail.com>
2017-12-12 12:45:45 -0600

    Added symbols to buttons.

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -8,20 +8,21 @@ public class Calculator implements ActionListener {
     private JTextField text;
     
     private JButton one = new JButton("1");
-    private JButton two = new JButton("1");
-    private JButton three = new JButton("1");
-    private JButton four = new JButton("1");
-    private JButton five = new JButton("1");
-    private JButton six = new JButton("1");
-    private JButton seven = new JButton("1");
-    private JButton eight = new JButton("1");
-    private JButton nine = new JButton("1");
-    private JButton zero = new JButton("1");
-    private JButton decimal = new JButton("1");
-    private JButton plus = new JButton("1");
-    private JButton minus = new JButton("1");
-    private JButton times = new JButton("1");
-    private JButton divide = new JButton("1");
+    private JButton two = new JButton("2");
+    private JButton three = new JButton("3");
+    private JButton four = new JButton("4");
+    private JButton five = new JButton("5");
+    private JButton six = new JButton("6");
+    private JButton seven = new JButton("7");
+    private JButton eight = new JButton("8");
+    private JButton nine = new JButton("9");
+    private JButton zero = new JButton("0");
+    private JButton decimal = new JButton(".");
+    private JButton plus = new JButton("+");
+    private JButton minus = new JButton("-");
+    private JButton times = new JButton("*");
+    private JButton divide = new JButton("/");
+    private JButton equals = new JButton("=");
 
     @Override
     public void actionPerformed(ActionEvent e) {
